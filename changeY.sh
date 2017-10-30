940c4e4144602185cb44811b7b38f7348412d55f
Your Name <you@example.com>
2017-10-30 16:07:37 +0800

    change Y

---
+++ changeY.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+#
+
+# git clone https://github.com/kubernetes-incubator/kubespray.git
+# cd kubespray
+# sh changY.sh
+
+
+## kubespray 2.1
+# grc_image_files=(
+# ./extra_playbooks/roles/dnsmasq/templates/dnsmasq-autoscaler.yml
+# ./extra_playbooks/roles/download/defaults/main.yml
+# ./extra_playbooks/roles/kubernetes-apps/ansible/defaults/main.yml
+# ./roles/download/defaults/main.yml
+# ./roles/dnsmasq/templates/dnsmasq-autoscaler.yml
+# ./roles/kubernetes-apps/ansible/defaults/main.yml
+# )
+
+## kubespray 2.3
+grc_image_files=(
+./extra_playbooks/roles/dnsmasq/templates/dnsmasq-autoscaler.yml.j2
+./extra_playbooks/roles/download/defaults/main.yml
+./extra_playbooks/roles/kubernetes-apps/ansible/defaults/main.yml
+./roles/download/defaults/main.yml
+./roles/dnsmasq/templates/dnsmasq-autoscaler.yml.j2 
+./roles/kubernetes-apps/ansible/defaults/main.yml
+)
+
+for file in ${grc_image_files[@]} ; do
+	# echo $file $file.bak
+	/bin/cp $file $file.bak
+	sed -i 's#gcr.io/google_containers#sugare#g' $file
+	sed -i 's#gcr.io/kubernetes-helm#sugare#g' $file
+done
